d1abfd2ac99df195eb82c317e067fdfc13ba1dcc
smartboy0310 <qdustudent6@gmail.com>
2022-07-16 11:47:51 +0500

    Edited search

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 const { Telegraf, session } = require('telegraf');
 require('dotenv').config();
-const { telegrafThrottler } = require('telegraf-throttler');
+
+const rateLimit = require('telegraf-ratelimit')
+
+
 const token = process.env.BOT_TOKEN;
 const bot = new Telegraf(token);
 
@@ -30,7 +33,7 @@ const stage = new Stage([
 
 bot.use(session());
 bot.use(stage.middleware());
-bot.use(telegrafThrottler());
+bot.use(rateLimit())
 
 bot.use(require('./Composer/start'));
 bot.use(require('./Composer/admin'));
@@ -42,7 +45,7 @@ bot.use(require('./Composer/category'));
 bot.use(require('./Composer/back'));
 bot.use(require('./Composer/subCategory'));
 
-
+bot.startPolling()
 bot.launch().then(() => {
 	console.log(`bot started on @${bot.options.username}`);
 });
