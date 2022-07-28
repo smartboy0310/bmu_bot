b357df6563da2260f55227bf2de67fc6969b7baa
smartboy0310 <qdustudent6@gmail.com>
2022-07-28 12:07:39 +0500

    Created answer Scene

--- Scene/questionSceneRu.js
+++ Scene/questionSceneRu.js
@@ -20,50 +20,56 @@ questionSceneRu.enter(async (ctx) => {
 });
 
 questionSceneRu.on('text', async (ctx) => {
-	await ctx.replyWithHTML(
-		`
-      <b>Ваш вопрос принят</b>
-      `,
-		Markup.keyboard(category.ru_keyboard).oneTime().resize().extra(),
-	);
-	let newQuestion = {};
-	const userId = ctx.update.message.from?.id;
-
-	const fs = new FS(path.resolve(__dirname, '..', 'data', 'users.json'));
-	const allUser = JSON.parse(fs.read());
-	const foundUser = allUser.find((e) => e.user_id == userId);
-
-	const phone = foundUser?.user_phone;
-	const name = foundUser?.user_name;
-
-	const question = ctx.update.message?.text;
-
-	const oldQuestion = new FS(
-		path.resolve(__dirname, '..', 'data', 'question.json'),
-	);
-	const allQuestion = JSON.parse(oldQuestion.read());
-
-	newQuestion.user_name = name;
-	newQuestion.user_phone = phone;
-	newQuestion.user_question = question;
-	newQuestion.question_number = allQuestion.length + 1;
-	newQuestion.user_id = userId;
-
-	allQuestion.push(newQuestion);
-
-	await ctx.telegram.sendMessage(
-		adminId,
-		`New Question:\n${question}\nQuestion number: ${allQuestion.length}`,
-		Markup.inlineKeyboard([
-			Markup.callbackButton('✏️ Javob Yozish', `${userId}`),
-		]).extra(),
-	);
-	await ctx.telegram.sendContact(adminId, `${phone}`, `${name}`);
-
-	new FS(path.resolve(__dirname, '..', 'data', 'question.json')).write(
-		allQuestion,
-	);
-	return ctx.scene.leave();
+	try {
+		await ctx.replyWithHTML(
+			`
+			<b>Ваш вопрос принят</b>
+			`,
+			Markup.keyboard(category.ru_keyboard).oneTime().resize().extra(),
+		);
+		let newQuestion = {};
+		const userId = ctx.update.message.from?.id;
+	
+		const fs = new FS(path.resolve(__dirname, '..', 'data', 'users.json'));
+		const allUser = JSON.parse(fs.read());
+		const foundUser = allUser.find((e) => e.user_id == userId);
+	
+		const phone = foundUser?.user_phone;
+		
+		const name = ctx.update.message.from?.first_name;
+	
+		const question = ctx.update.message?.text;
+	
+		const oldQuestion = new FS(
+			path.resolve(__dirname, '..', 'data', 'question.json'),
+		);
+		const allQuestion = JSON.parse(oldQuestion.read());
+	
+		newQuestion.user_name = name;
+		newQuestion.user_phone = phone;
+		newQuestion.user_question = question;
+		newQuestion.question_number = allQuestion.length + 1;
+		newQuestion.user_id = userId;
+	
+		allQuestion.push(newQuestion);
+	
+		await ctx.telegram.sendMessage(
+			adminId,
+			`Yangi savol:\n${question}\nSavol egasi: ${name}\nSavol raqami: ${allQuestion.length}`,
+			Markup.inlineKeyboard([
+				Markup.callbackButton('✏️ Javob Yozish', `${userId}`),
+			]).extra(),
+		);
+		//await ctx.telegram.sendContact(adminId, `${phone}`, `${name}`);
+	
+		new FS(path.resolve(__dirname, '..', 'data', 'question.json')).write(
+			allQuestion,
+		);
+		return ctx.scene.leave();
+		
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 module.exports = questionSceneRu;
